372d152d94f549b4b074fe8619cd334f38dda6f6
richardpphilip <richardpphilip1@gmail.com>
2021-05-03 10:08:58 -0700

    fixed bug in collection application.  tested in postman and GET works as intended

---
+++ flashcard_backend/collection/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('collection/', views.CollectionList.as_view())
+]
--- flashcard_backend/collection/views.py
+++ flashcard_backend/collection/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Collection
+from .serializers import CollectionSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+class CollectionList(APIView):
+    def get(self, request):
+        collection = Collection.objects.all()
+        serializer = CollectionSerializer(collection, many=True)
+        return Response(serializer.data)
+    def post(self, request):
+        serializer = CollectionSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
